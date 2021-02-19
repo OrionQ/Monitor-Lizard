29a8d163a3e2d5430906fa2dc855c05d390278a6
msleonar <msleonar@ncsu.edu>
2021-02-19 12:35:03 -0500

    Address michael feedback

--- web/models.py
+++ web/models.py
@@ -1,30 +1,57 @@
 from django.db import models
 
+# Models for the MonitorLizard web application
+
 
 class Metric(models.Model):
+    """A Quantity measured on a host machine"""
     METRIC_TYPES = (
-        ('perc', 'Percent'),
-        ('count', 'Count'),
-        ('bits', 'Bits'),
+        ('int', 'Integer'),
+        ('float', 'Floating'),
+        ('array', 'List'),
     )
-    category = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    metric_type = models.CharField(max_length=5, choices=METRIC_TYPES)
-    description = models.TextField()
+    category = models.TextField()
+    name = models.TextField()
+    metric_type = models.TextField(choices=METRIC_TYPES)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['category', 'name']),
+        ]
 
 
 class Host(models.Model):
+    """A host that reports to the MonitorLizard system"""
     guid = models.UUIDField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['guid']),
+        ]
 
 
 class HostTags(models.Model):
+    """A collection of hosts"""
     hosts = models.ManyToManyField(Host)
-    name = models.CharField(max_length=200)
-    registration_key = models.CharField(max_length=256)
+    name = models.TextField()
+    registration_key = models.TextField(null=True, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['registration_key']),
+        ]
 
 
 class Report(models.Model):
+    """A report from a host containing the value of a specific metric"""
     time = models.DateTimeField()
-    metric = models.ForeignKey(Metric, on_delete=models.PROTECT)
-    host = models.ForeignKey(Host, on_delete=models.PROTECT)
+    metric = models.ForeignKey(Metric, on_delete=models.SET_NULL)
+    host = models.ForeignKey(Host, on_delete=models.SET_NULL)
     value = models.JSONField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['time']),
+        ]
