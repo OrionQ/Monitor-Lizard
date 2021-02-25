7a1f66ae70ca747d8b91ec2f6ccf19c1a15e9698
msleonar <msleonar@ncsu.edu>
2021-02-25 14:57:55 -0500

    Make working on models on admin easier

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Metric, Host, HostTag, Report, Team, User, AlertRule, Alert
+
+# Registering models for the admin page
+
+admin.site.register(Metric)
+admin.site.register(Host)
+admin.site.register(HostTag)
+admin.site.register(Report)
+admin.site.register(Team)
+admin.site.register(User)
+admin.site.register(AlertRule)
+admin.site.register(Alert)
