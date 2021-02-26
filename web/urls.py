254236a09ccdae0bb3c00b889e6a01e36bdfb78c
srgade <srgade@ncsu.edu>
2021-02-25 20:46:23 -0500

    Added templates
    Initial templates for host, tag, and dashboard

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home),
+    path('tag/', views.tag),
+    path('host/', views.host),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're viewing the monitor lizard website.")
+def home(request):
+    # Returning a template
+    return render(request, 'web/dashboard.html')
+
+def tag(request):
+    return render(request, 'web/tag.html')
+
+def host(request):
+    return render(request, 'web/host.html')
