d4962b1e4e40227b4460906a7d6b58e75b1133ba
srgade <srgade@ncsu.edu>
2021-03-01 18:09:53 -0500

    Functionality for user authentication
    Added login page, redirected to this page when incorrect credentials are entered, updated nav bar to display logout link & name of logged in user

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('', views.home, name="home"),
     path('tag/', views.tag),
     path('host/', views.host),
 ]
--- web/views.py
+++ web/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def loginPage(request):
+    if request.method == 'POST':
+        # Grab the username and password
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        # Log the user in
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Incorrect Credentials")
+    context = {}
+    return render(request, 'web/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
 def home(request):
     # Returning a template
     return render(request, 'web/dashboard.html')
