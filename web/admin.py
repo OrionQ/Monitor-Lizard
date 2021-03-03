eebe8640a1b16b6b15691996ce0dcefd2df5af76
srgade <srgade@ncsu.edu>
2021-03-03 13:51:16 -0500

    Added host column to alerts table
    Imported models and set routing to Dashboards page as well

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Host)
+admin.site.register(Metric)
+admin.site.register(HostTag)
+admin.site.register(Report)
+admin.site.register(Team)
+admin.site.register(User)
+admin.site.register(AlertRule)
+admin.site.register(Alert)
