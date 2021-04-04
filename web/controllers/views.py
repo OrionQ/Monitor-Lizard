9dcf9124ebf1ff7263e7cdced222dd380b6e3dce
srgade <srgade@ncsu.edu>
2021-04-03 21:59:26 -0500

    Added table with report data
    Table is populated with data from the database as reports come in (keeps track of metric, host name, timestamp, & value)

--- web/controllers/views.py
+++ web/controllers/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from web.models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def loginPage(request):
@@ -30,8 +32,14 @@ def logoutUser(request):
 
 @login_required(login_url='login')
 def home(request):
+    report_list = Report.objects.all()
+
+    paginator = Paginator(report_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     # Returning a template
-    return render(request, 'web/dashboard.html')
+    return render(request, 'web/dashboard.html', {'page_obj': page_obj})
 
 @login_required(login_url='login')
 def tag(request):
