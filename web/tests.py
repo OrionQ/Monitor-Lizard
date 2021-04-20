32c66c452fafe48ba974a0946ce4e14e71b7241b
Rose Zendels <elzendel@ncsu.edu>
2021-04-20 16:14:30 -0400

    Added initial automated frontend testing

--- web/tests.py
+++ web/tests.py
@@ -1,13 +1,57 @@
-from django.test import TestCase
-import models
+from django.test import TestCase, Client
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import web.models as models
+import web.controllers
+import ast
 
 # Create your tests here.
-class ModelsTestCase(TestCase):
+
+
+class WebTestCase(TestCase):
     def setUp(self):
-        models.objects.create(name="cpu_usage",category="cpu",metric_type="Integer")
-        
-    def test_models_have_correct_fields(self):
-        cpu = models.objects.get(name="cpu_usage")
-        self.assertEqual(cpu.name(), "cpu_usage")
-        self.assertEqual(cpu.category(), "cpu")
-        self.assertEqual(cpu.metric_type(), "Integer")
+        user = User.objects.create_user('admin', password='pass')
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        testHost = models.Host.objects.create(guid="eb651255-e95c-41e8-8bf6-5c405eaf5061")
+        testHost.save()
+        testTag = models.HostTag.objects.create(name="Unsorted")
+        testTag.hosts.set([testHost])
+
+    def testFakeLogin(self):
+        c = Client()
+        #try logging into a fake user
+        response = c.post('/login/', {'username': 'fakeadmin', 'password': 'fakepass'})
+        self.assertEqual(response.status_code, 200)
+        #assert that we can't access pages, redirect back to login
+        response = c.get('/tag/Unsorted/')
+        self.assertRedirects(response,'/login/?next=/tag/Unsorted/')
+
+    def testLoginLogout(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'admin', 'password': 'pass'})
+        self.assertEqual(response.status_code, 302)
+        response = c.get('/logout/')
+        self.assertRedirects(response,'/login/')
+
+    def testTagPage(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'admin', 'password': 'pass'})
+        self.assertEqual(response.status_code, 302)
+        response = c.get('/tag/Unsorted/')
+        self.assertEqual(response.status_code, 200)
+
+    def testHostPage(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'admin', 'password': 'pass'})
+        self.assertEqual(response.status_code, 302)
+        response = c.get('/host/eb651255-e95c-41e8-8bf6-5c405eaf5061/')
+        self.assertEqual(response.status_code, 200)
+
+    def testRegisterHost(self):
+        c = Client()
+        response = c.post('/api/host/')
+        self.assertEqual(response.status_code, 200)
+        responseJson = ast.literal_eval(response.content.decode("UTF-8"))
+        self.assertIn('guid',responseJson)
